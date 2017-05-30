5cf780dee2e9b05adf60bd8964dee9a841cf8f1c
scotteskridge <scott.eskridge@gmail.com>
2017-05-30 15:43:39 -0700

    caps problems?

--- client/src/app/appointment.service.ts
+++ client/src/app/appointment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Http, Headers, RequestOptions } from "@angular/http"
-import { User } from "./User"
+import { User } from "./user"
 import "rxjs"
 
 
